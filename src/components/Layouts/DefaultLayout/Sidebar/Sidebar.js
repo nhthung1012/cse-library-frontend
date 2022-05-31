eb7a0b822fcb18139d538184e3129e5eca0dee90
hungg1012 <nhthung1012@gmail.com>
2022-05-31 19:34:14 +0700

    add icon in Sidebar

--- src/components/Layouts/DefaultLayout/Sidebar/Sidebar.js
+++ src/components/Layouts/DefaultLayout/Sidebar/Sidebar.js
@@ -1,5 +1,14 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faHouse,
+    faSquareCheck,
+    faListCheck,
+    faChartColumn,
+    faUserGroup,
+    faBookOpen,
+} from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -7,17 +16,33 @@ function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
             <div className={cx('sidebar-img')}>
+                <FontAwesomeIcon icon={faBookOpen} />
                 <h1>CSE Library</h1>
             </div>
             <div className={cx('sidebar-link')}>
-                <a href="/">Trang chủ</a>
-                <a href="/ViTri">Chọn vị trí</a>
-                <a href="/QuetThe">Quẹt thẻ</a>
-                <a href="/ThongKe">Thống kê</a>
-                <a href="/Manage">Quản Lý</a>
+                <a href="/">
+                    <FontAwesomeIcon icon={faHouse} />
+                    <b>{' Trang chủ'}</b>
+                </a>
+                <a href="/ViTri">
+                    <FontAwesomeIcon icon={faSquareCheck} />
+                    <b>{' Chọn vị trí'}</b>
+                </a>
+                <a href="/QuetThe">
+                    <FontAwesomeIcon icon={faListCheck} />
+                    <b>{' Quẹt thẻ'}</b>
+                </a>
+                <a href="/ThongKe">
+                    <FontAwesomeIcon icon={faChartColumn} />
+                    <b>{' Thống kê'}</b>
+                </a>
+                <a href="/Manage">
+                    <FontAwesomeIcon icon={faUserGroup} />
+                    <b>{' Quản Lý'}</b>
+                </a>
             </div>
             <a className={cx('sidebar-signout')} href="/">
-                Đăng xuất
+                {' Đăng xuất'}
             </a>
         </aside>
     );
