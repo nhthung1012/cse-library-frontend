c2e03bf539256a9b4a9c8020013f674eb6940162
Binecsbk20 <bientran@MacBook-Pro-cua-Bien.local>
2022-06-06 18:31:57 +0700

    update sidebar, thongke, bieudo

--- src/pages/Liststatistics/Liststa.js
+++ src/pages/Liststatistics/Liststa.js
@@ -1,12 +1,37 @@
 import classNames from 'classnames/bind';
 import styles from './Liststa.module.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
+var studentinfor = {
+    name: 'Trần Long Biên',
+    mssv: '2010924',
+    checkintime: '11:56 am',
+    checkouttime: '12:21 am'
+}
+var liststudnets = [studentinfor, studentinfor, studentinfor, studentinfor, studentinfor, studentinfor,studentinfor,studentinfor,studentinfor,studentinfor,studentinfor ];
+
+
 
 function List_sta() {
+    const [year, setYear] = useState(null)
+    const [month, setMonth] = useState(null)
+    const [day, setDay] = useState(null)
+    const [search, setSearch] = useState(null)
+    function getYear (val){
+        setYear(val.target.value)
+    }
+    function getMonth (val){
+        setMonth(val.target.value)
+    }
+    function getDay (val){
+        setDay(val.target.value)
+    }
+    function getSearch (val){
+        setSearch(val.target.value)
+    }
     return (
         <>
             <div className={cx('top-wrapper')}>
@@ -26,139 +51,57 @@ function List_sta() {
                 <div className={cx('day-month-year-state')}>
                     <p>Trang thống kê sinh viên vào/ra thư viện </p>
                     <div className={cx('day-month-year')}>
-                        <button className={cx('year-wrapper')}>
-                            Năm
-                            <div className={cx('angle-down-ic')}>
-                                <FontAwesomeIcon icon={faAngleDown} />{' '}
-                            </div>
-                        </button>
+                        <div className={cx('dmy-wrapper-chart')}>
+                            <input type="text" placeholder="Năm" spellCheck={false} onChange = {getYear} />
+                            {year}
+                        </div>
 
-                        <button className={cx('month-wrapper')}>
-                            Tháng
-                            <div className={cx('angle-down-ic')}>
-                                <FontAwesomeIcon icon={faAngleDown} />{' '}
-                            </div>
-                        </button>
+                        <div className={cx('dmy-wrapper-chart')}>
+                            <input type="text" placeholder="Tháng" spellCheck={false} onChange = {getMonth} />
+                            {month}
+                        </div>
 
-                        <button className={cx('day-wrapper')}>
-                            Ngày
-                            <div className={cx('angle-down-ic')}>
-                                <FontAwesomeIcon icon={faAngleDown} />{' '}
-                            </div>
-                        </button>
+                        <div className={cx('dmy-wrapper-chart')}>
+                            <input type="text" placeholder="Ngày" spellCheck={false} onChange = {getDay} />
+                            {day}
+                        </div>
 
                         <div className={cx('search-wrapper')}>
-                            <input placeholder="Tìm kiếm" spellCheck={false} />
+                            <input type = "text" placeholder="Tìm kiếm" spellCheck={false} onChange = {getSearch} />
+
                             <button className={cx('search-wrapper-button')}>
                                 <div className={cx('magnifying-glass')}>
                                     <FontAwesomeIcon icon={faMagnifyingGlass} />{' '}
                                 </div>
                             </button>
-                        
                         </div>
+                        {search}
                     </div>
                 </div>
             </div>
-
-            <div className={cx('student-list')}>
-                <div className={cx('student')}>
-                    <div className={cx('student-avatar')}></div>
-                    <div className={cx('student-info')}>
-                        <div className={cx('student-id')}>2012345</div>
-                        <div className={cx('student-name')}>
-                            Nguyễn Ngọc Anh
-                            <div className={cx('student-info-time')}>
-                                <div className={cx('time-in')}>
-                                    Check-in: 11:56am
-                                </div>
-                                <div className={cx('time-out')}>
-                                    Check-out: 11:56am
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button className={cx('student-fix')}>Chỉnh sửa</button>
-                </div>
-            </div>
-            <div className={cx('student-list')}>
-                <div className={cx('student')}>
-                    <div className={cx('student-avatar')}></div>
-                    <div className={cx('student-info')}>
-                        <div className={cx('student-id')}>2012345</div>
-                        <div className={cx('student-name')}>
-                            Nguyễn Ngọc Anh
-                            <div className={cx('student-info-time')}>
-                                <div className={cx('time-in')}>
-                                    Check-in: 11:56am
-                                </div>
-                                <div className={cx('time-out')}>
-                                    Check-out: 11:56am
+            <div>
+                {liststudnets.map((stu) => {
+                    return <div className={cx('student-list')}>
+                        <div className={cx('student')}>
+                            <div className={cx('student-avatar')}></div>
+                            <div className={cx('student-info')}>
+                                <div className={cx('student-id')}>{stu.mssv}</div>
+                                <div className={cx('student-name')}>
+                                    {stu.name}
+                                    <div className={cx('student-info-time')}>
+                                        <div className={cx('time-in')}>
+                                            Check-in: {stu.checkintime}
+                                        </div>
+                                        <div className={cx('time-out')}>
+                                            Check-out: {stu.checkouttime}
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+                            <button className={cx('student-fix')}>Chỉnh sửa</button>
                         </div>
-                    </div>
-                    <button className={cx('student-fix')}>Chỉnh sửa</button>
-                </div>
-            </div>
-            <div className={cx('student-list')}>
-                <div className={cx('student')}>
-                    <div className={cx('student-avatar')}></div>
-                    <div className={cx('student-info')}>
-                        <div className={cx('student-id')}>2012345</div>
-                        <div className={cx('student-name')}>
-                            Nguyễn Ngọc Anh
-                            <div className={cx('student-info-time')}>
-                                <div className={cx('time-in')}>
-                                    Check-in: 11:56am
-                                </div>
-                                <div className={cx('time-out')}>
-                                    Check-out: 11:56am
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button className={cx('student-fix')}>Chỉnh sửa</button>
-                </div>
-            </div>
-            <div className={cx('student-list')}>
-                <div className={cx('student')}>
-                    <div className={cx('student-avatar')}></div>
-                    <div className={cx('student-info')}>
-                        <div className={cx('student-id')}>2012345</div>
-                        <div className={cx('student-name')}>
-                            Nguyễn Ngọc Anh
-                            <div className={cx('student-info-time')}>
-                                <div className={cx('time-in')}>
-                                    Check-in: 11:56am
-                                </div>
-                                <div className={cx('time-out')}>
-                                    Check-out: 11:56am
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button className={cx('student-fix')}>Chỉnh sửa</button>
-                </div>
-            </div>
-            <div className={cx('student-list')}>
-                <div className={cx('student')}>
-                    <div className={cx('student-avatar')}></div>
-                    <div className={cx('student-info')}>
-                        <div className={cx('student-id')}>2012345</div>
-                        <div className={cx('student-name')}>
-                            Nguyễn Ngọc Anh
-                            <div className={cx('student-info-time')}>
-                                <div className={cx('time-in')}>
-                                    Check-in: 11:56am
-                                </div>
-                                <div className={cx('time-out')}>
-                                    Check-out: 11:56am
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button className={cx('student-fix')}>Chỉnh sửa</button>
-                </div>
+                    </div>;
+                })}
             </div>
         </>
     );
